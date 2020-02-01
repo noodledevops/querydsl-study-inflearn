3b013ae4ed4549c2f953569079785eaffcfbecdb
gskjhyoo <gskjhyoo@naver.com>
2020-02-01 16:28:27 +0900

    6강 스프링 부트 설정 - JPA, DB 5강 H2 데이터베이스 설치 4강 라이브러리 살펴보기

--- src/test/java/study/querdsl/QuerdslApplicationTests.java
+++ src/test/java/study/querdsl/QuerdslApplicationTests.java
@@ -5,14 +5,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querdsl.entity.Hello;
 import study.querdsl.entity.QHello;
 
 import javax.persistence.EntityManager;
 
-@SpringBootTest
-@Transactional
+@SpringBootTest	
+@Transactional 	// 기본적으로 다 롤백
+@Commit		 	// 공부할때 테스트는 남기지만 실전에서는 메모리에서만 돌려서 확인하는게 좋다
 class QuerdslApplicationTests {
 
 	@Autowired
